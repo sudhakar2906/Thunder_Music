04196af942b4f11e90b8b2b2a4fcd1a800b4138e
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-03-29 21:44:10 +0530

    Update tgcalls.py

--- tgcalls/tgcalls.py
+++ tgcalls/tgcalls.py
@@ -17,7 +17,7 @@ def on_stream_end(chat_id: int) -> None:
         pytgcalls.leave_group_call(chat_id)
     else:
         pytgcalls.change_stream(
-            chat_id, sira.get(chat_id)["file_path"]
+            chat_id, queue.get(chat_id)["file_path"]
         )
 
 
